94f304e8daec1d92d65ac92be7960fdb435fbeb0
Mazzocco <mazzocco.jc@icloud.com>
2020-08-16 22:10:47 +0200

    Création des fichiers templates et faq

---
+++ faq.php
@@ -0,0 +1,205 @@
+<!doctype html>
+
+<!--derniere modification: JC le 14/08 à 10h08 -->
+
+<html lang="fr">
+<head>
+	
+	<meta charset="UTF-8">
+	<meta name="description" content="Toutes l'actualité de du magasin">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/cookieconsent.min.css">
+	
+	<title>SAFPICSTORE - L'actualité de la boutique</title>
+	
+	<!--Intégration du favicon -->
+	<link rel="manifest" href="assets/js/manifest.json">
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon//apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="assets/images/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	
+	<!-- Fin d'intégration du favicon -->
+	
+</head>
+
+<body>
+
+<!-- Début du header-->
+
+<div class="container-fluid" id="navcolor">
+  <header class="d-flex justify-content-around"> 
+    
+ <?php include('assets/inc/entete.php'); ?>
+ 
+	</header>
+</div>
+ 
+<div class="container-fluid">
+   
+	
+
+	
+<div class="container">
+	  
+ 		
+    <figure class="text-center">
+	<img src="assets/images/logos/logo-safpic-fond-transparent.png" class="img-fluid centrage-image padtop" alt="Logo de l'association SAFPIC"/>
+	</figure>
+	   
+	   
+        <h1 class="padtop text-center">Foire aux questions</h1>
+
+<p>Pourquoi plus d’articles sont disponibles dans la boutique en ligne SAFPIC que dans le magasin ?</p>
+	
+<p>L’espace de présentation et le stock d’un vendeur sont généralement limités. En tant que magasin associatif, la boutique en ligne SAFPIC dispose de capacités de stockage plus importantes. C’est pourquoi nous souhaitons et pouvons vous proposer la gamme complète de chaque collection, du moins tant que les différents articles ne sont pas épuisés.</p>
+
+<p>Livrez-vous également à l’étranger ?</p>
+<p>La livraison de nos colis s'effectue en France métropolitaine, y compris la Corse et Monaco, à l’exclusion des DOM-TOM.
+La conclusion du contrat s'effectue en français et vous recevrez tous les documents en français.</p>
+
+<p>Quels avantages ai-je à m’enregistrer (=ouverture d’un compte client) ?</p>
+
+<p>En tant que client(e) enregistré(e), vous pouvez faire vos achats plus facilement et plus rapidement dans la boutique en ligne Street One. Vos coordonnées sont déjà saisies sur la page de commande et vous pouvez enregistrer des adresses de livraison supplémentaires. Si vous choisissez de vous abonner à notre newsletter, vous recevez par e-mail des offres spéciales sélectionnées et êtes toujours informé(e)s. Votre compte client vous permet de pouvoir contrôler complètement vos commandes et vos données personnelles</p>
+
+<p>Dans quelle mesure mes données sont-elles protégées ?</p>
+	
+<p>Nous attachons une grande importance à la protection de vos données personnelles. Nous respectons évidemment les strictes dispositions de la loi française sur la protection des données (RGPD).</p>
+	
+<p>Vos données personnelles sont toujours cryptées par un système de sécurité numérique avant de nous être transmises. Des mesures techniques protègent nos pages Internet contre tout dommage, toute destruction et tout accès non autorisé. Tous les collaborateurs qui accèdent à vos données ont signé une déclaration de confidentialité absolue.
+Votre confiance nous importe énormément. Vous pouvez donc être sûr que SAFPIC vous propose toujours les dernières tendances, mais aussi que vos données sont en sécurité.</p>
+	
+<p>Je ne peux pas me connecter à la boutique en ligne SAFPIC. Que faire ?</p>
+
+<p>Avez-vous saisi l’adresse e-mail et le mot de passe corrects ? Si vous avez oublié votre mot de passe, demandez en un nouveau. Ou écrivez un e-mail à notre équipe du service clientèle à <a href="mailto:contact@safpic-asso.org">contact@safpic-asso.org</a>.</p>
+	
+<p>Existe-t-il un montant de commande minimum ou un montant maximum de commande?</p>
+	
+<p>Il n’y a pas de minimum de commande chez nous, il est donc tout à fait possible de commander un seul article. Pour toute commande de 24 € ou plus, vous profitez de la livraison à seulement 3 €. Pour des raisons de sécurité, le montant maximal est fixé à 300 € par commande.</p>
+	
+<p>Le paiement en ligne est-il sécurisé ?</p>
+	
+<p>Nous attachons une grande importance à la sécurité lors des transactions. Vos données sont transmises uniquement sous forme cryptée, de sorte que des tiers ne puissent pas les lire. Votre paiement par carte de crédit est traité par un partenaire certifié en matière de sécurité.</p>
+	
+<p>Comment puis-je payer ?</p>
+	<p>Les modes de paiement suivants sont actuellement disponibles :</p>
+	
+	<ul>
+		<ol>
+			<li><p>Paiement préalable : Nous réservons les articles souhaités jusqu’à sept jours à compter du passage de la commande. Vous pouvez ainsi virer ou déposer en toute facilité le montant à payer sur notre compte. Vous trouverez nos coordonnées bancaires dans la confirmation de commande que vous recevez par e-mail directement après l’envoi de votre commande. Lors du virement, veillez à ce que les données saisies soient complètes et correctes afin que nous puissions identifier correctement votre virement et que vous receviez rapidement votre commande. Dès que le montant de la facture est intégralement crédité sur notre compte, nous pouvons traiter, emballer et expédier votre commande. Nous vous en informons de nouveau par e-mail (confirmation de l’expédition). Vous recevez votre colis en règle générale sous 2 à 3 jours ouvrables</p>
+		</li>			
+			<li><p>Carte de crédit : Le mode de paiement le plus simple. Nous acceptons les cartes de crédit Visa et Mastercard. La boutique n’enregistre pas les données de votre carte de crédit</p></li>
+			<li><p>PayPal : Vous avez un compte PayPal ? Dans ce cas, n'hésitez pas à utiliser ce mode de paiement. Sinon, ouvrez un compte PayPal sur www.paypal.fr. Ce compte vous permettra également à l'avenir de faire des achats rapidement et en toute sécurité dans de nombreuses boutiques en ligne internationales.</p></li>
+			<li><p>Carte Bleue : Régler votre commande par Carte Bleue est très facile et confortable. Pour payer avec la Carte Bleue, sélectionnez Carte Bleue comme mode de paiement lors de l’exécution de votre commande. Via l'environnement sécurisé de Carte Bleue vous pouvez effectuer et confirmer le paiement. C'est tout, le paiement est déjà terminé.</p></li>
+		</ol>
+	</ul>
+	
+
+<p>À combien s’élèvent les frais d’expédition ?</p>
+	
+<p>La livraison à 0,99 € est très avantageuse. Toutefois, pour toute commande de moins de 24 €, nous facturons 3,95 € de frais d’envoi (TVA incl.), peu importe le nombre de colis.</p>
+
+<p>Quand vais-je recevoir ma commande ?</p>
+	
+<p>La livraison a généralement lieu sous 3-4 jours ouvrables à compter de notre confirmation de commande. Si vous n’avez pas reçu votre livraison sous six jours ouvrables ou si vous n’avez pas reçu de confirmation d’expédition, veuillez écrire un e-mail à boutique.safpic@safpic-asso.org. N’oubliez pas d’indiquer votre numéro de client et/ou le numéro de commande.</p>
+	
+<p>Comment puis-je voir ce qu’il en est de ma commande ?</p>
+
+<p>Si vous vous êtes enregistré à la boutique en ligne, vous pouvez consulter vos données personnelles et vos données de commande sous votre "compte SAFPIC" après vous être connecté. Pour chaque commande, vous êtes informé si la commande est déjà payée ou si elle est en cours d’expédition. Nous vous envoyons également un e-mail lorsque nous recevons votre commande et un second e-mail dès que la marchandise quitte notre stock (confirmation d’expédition).
+Si vous n’êtes pas enregistré, vous ne pouvez pas consulter ultérieurement votre commande en ligne. Vous serez cependant toujours informé par e-mails.</p>
+	
+<p>Comment puis-je annuler ma commande ?</p>
+
+<p>Vous pouvez annuler votre commande en faisant usage de votre droit de révocation légal. Il suffit de nous envoyer un e-mail ou, si la marchandise a déjà été expédiée, de nous la retourner directement en refusant de la réceptionner. Vous trouverez tous les détails sur le droit de révocation légal dans nos CGV, au paragraphe « Droit de révocation ».</p>
+	
+<p>Quels articles puis-je retourner ? Et comment ? De quoi faut-il tenir compte lors de l’essayage ? Qui prend en charge les frais de retour et comment le remboursement du prix d’achat est-il effectué ?</p>
+	
+<p>Vous pouvez retourner chaque article que vous avez acheté dans la boutique en ligne SAFPIC dans les 14 jours suivant sa réception, en faisant valoir votre droit de révocation légal. Vous trouverez tous les détails sur le droit de révocation légal dans nos CGV, au paragraphe « Droit de révocation ». Voici ce que nous recommandons en pratique :
+Évitez d’endommager ou de salir la marchandise lors de l’ouverture de l’emballage et du test téléphone ou accessoire. Si possible, ne retirez les étiquettes, etc. qu’après avoir décidé de conserver la marchandise. Vous risquez sinon de devoir payer des frais de dédommagements (par ex. frais de reconditionnement emballage).</p>
+	
+<p>Si vous décidez de retourner la marchandise, veuillez y joindre les étiquettes déjà retirées. Veuillez utiliser l’emballage d’origine pour protéger la marchandise ou emballez la marchandise de manière à ce qu’elle ne puisse pas être endommagée pendant le transport. Pour faciliter l’attribution de votre commande, veuillez utiliser le bon de retour joint à votre commande. Il vous suffit de cocher sur ce bon l’article retourné. Vous nous aidez beaucoup en indiquant le motif du retour. Nous pouvons ainsi apprendre et nous améliorer. En plus du bon de retour, une étiquette autocollante avec l’adresse de retour est jointe à la livraison. Veuillez utiliser cette étiquette pour votre retour. Veuillez également tenir compte d’éventuelles remarques complémentaires de votre service de livraison pour le déroulement des retours. Veuillez noter que ces recommandations ne constituent aucune condition préalable à l’exercice de votre droit de révocation.</p>
+	
+<p>L’adresse de retour est l’adresse suivante (si vous n’utilisez pas l’étiquette autocollante) :
+<p><b>SAFPIC Association<br>3 Rond-point des Acacias,<br>Centre équestre des 3D, 59790 RONCHIN</b></p>
+
+<p>Veuillez impérativement joindre le bon de retour ou un courrier spécifiant le numéro de facture ou votre numéro de client (en cas de paiement préalable, veuillez également indiquer vos coordonnées bancaires) afin que le retour vous soit attribué sans problème et que le prix d’achat vous soit remboursé.</p>
+	
+<p>Si vous faites usage de votre droit de révocation dans le délai de 14 jours, aucun frais de retour ne vous est facturé. Condition : le prix d’achat (sans frais supplémentaires) de la marchandise retournée est supérieur à 40 euros et vous avez déjà payé entièrement la marchandise. Vous trouverez de plus amples informations dans nos CGV.</p>
+
+
+<p>Nous n’acceptons aucun retour de marchandise légitime, notamment lorsque:</p>
+
+	<ul>
+		<ol>
+			<li>le délai de révocation de 14 jours est écoulé;</li>
+			<li>la marchandise n’a pas été achetée dans notre boutique en ligne SAFPIC ou la marchandise achetée n’est pas comprise dans le colis de retour ;</li>
+			<li>lorsqu’il y a une tentative de fraude évidente.</li>
+		</ol>
+	</ul>
+
+
+En cas d’un retour de marchandise non légitime, nous prenons contact avec vous
+Dès que la marchandise retournée arrive à notre boutique associative, elle est vérifiée et un avoir est crédité. En fonction du mode de paiement choisi, votre carte de crédit ou votre compte Paypal est crédité du montant correspondant. En cas de paiement préalable, nous virons le montant sur le compte bancaire à partir duquel vous avez payé ou sur le compte indiqué séparément dans votre courrier.
+En règle générale, le prix d’achat vous est remboursé en l’espace d’une semaine après la réception de la marchandise retournée.
+Comment puis-je contacter SAFPIC pour tous sujets concernant mon achat en ligne?
+Cliquez simplement sur " Contact "dans la boutique en ligne. Vous pouvez poser vos questions à notre équipe du service clientèle via le formulaire de contact.
+Comment contacter SAFPIC à propos d'autres sujets?
+Dans notre boutique en ligne, cliquez sur le lien « Contact ». Via le formulaire de contact, vous pouvez poser vos questions à notre équipe du Service clientèle. Nous transfèrerons votre question directement au bon interlocuteur.
+Mon nom ou mon adresse a changé. Que dois-je faire?
+Une fois que vous êtes connecté à la boutique en ligne sur le site Street One, cliquez simplement sur « Compte SAFPIC » en haut à droite et modifiez votre nom et votre adresse.
+Vous trouverez de plus amples informations dans nos CGV .
+</p>
+	  
+                               
+		 
+				
+		   	   
+</div>
+
+<?php include('assets/inc/footer.php'); ?>
+  
+      <script src="assets/js/jquery-3.5.1.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/js/cookieconsent.min.js" data-cfasync="false"></script>
+  
+  <script>
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#1d8a8a"
+      },
+      "button": {
+        "background": "transparent",
+        "text": "#62ffaa",
+        "border": "#62ffaa"
+      }
+    },
+    "position": "bottom-left",
+    "content": {
+      "message": "Attention: Ce site n'est pas un site e-commerce, c'est un devoir d'évaluation pour le centre WF3 de Lille. Nous sommes des étudiants et utilisons des cookies pour comprendre l'analyse d'audience d'un site internet. ",
+      "dismiss": "OK",
+      "link": "privacy-policy.php",
+      "href": "https://www.demo-projet-ecole.net/privacy-policy"
+    }
+  });
+  </script>
+  
+  </body>
+  
+  </html>
+
